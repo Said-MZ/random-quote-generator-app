d99554eb0e2bcbf9bad75f03d3bf6170008414d0
Said zain <saidzain897@gmail.com>
2023-12-31 03:37:20 +0300

    added tweet button, icons, optimised  css
    added a button that will allow user to tweet quote automatically, added icons to the button,and optimised and modified css.

--- src/Generator.jsx
+++ src/Generator.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { BiSolidQuoteRight } from "react-icons/bi";
+import { FaXTwitter } from "react-icons/fa6";
+import { RiAiGenerate } from "react-icons/ri";
 
 const Generator = ({ quote, generateQuote }) => {
+  const tweetQuote = () => {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+      `"${quote.quote}" - ${quote.person}`
+    )}`;
+    window.open(twitterUrl, "_blank");
+  };
+
   return (
     <main>
       <section className="generator">
@@ -14,7 +23,16 @@ const Generator = ({ quote, generateQuote }) => {
         </div>
         <div className="btn-container">
           <button className="btn" onClick={generateQuote}>
-            New Quote
+            <div>
+              <RiAiGenerate />
+              <span>New Quote</span>
+            </div>
+          </button>
+          <button className="btn" onClick={tweetQuote}>
+            <div>
+              <FaXTwitter />
+              <span>Tweet Quote</span>
+            </div>
           </button>
         </div>
       </section>
